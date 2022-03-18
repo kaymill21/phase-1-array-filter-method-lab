ce6f92ba9bbcf5afe5d3b7a128766872f7948198
kayla <kay.ann2000@gmail.com>
2022-03-18 08:30:39 -0600

    commit tree 46fad442cb1f40d7e1807283b4d054562a9e7173

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+//const drivers = ['Bobby', 'saMMy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+console.log(('saMMy').toUpperCase())
+console.log(('sammy').toUpperCase())
+
+function findMatching(drivers,string) {
+
+    let answer = drivers.filter(el => el.toLowerCase() === string.toLowerCase())
+    //let drivers = string.toLowerCase();
+
+    console.log('Bobby'.toLowerCase())
+      return answer
+}
+   
+function fuzzyMatch(drivers, string) {
+    return drivers
+      .map((driver) => driver.split())
+      .filter((splitDriver) => splitDriver[0].indexOf(string) === string);
+  }
+//   function fuzzyMatch(drivers, string){
+//     const fuzzyDrivers = drivers.filter(function(person){
+//         return person.slice(0) == string.slice(0);
+//     });
+//     return fuzzyDrivers;
